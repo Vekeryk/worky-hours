4e396e83946090c203caf7e9d32304445af358a2
Denys Vekeryk <den.vekeryk@gmail.com>
2025-02-02 13:56:08 +0200

    fix(core): disable node integration, devTools and resizing window

--- src/main/main.js
+++ src/main/main.js
@@ -2,18 +2,21 @@ const { app, BrowserWindow } = require("electron");
 const path = require("node:path");
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+  const isDev = !app.isPackaged;
+
+  const mainWindow = new BrowserWindow({
     title: "Worky Hours",
     width: 500,
     height: 600,
+    resizable: false,
     icon: path.join(__dirname, "assets", "icon.png"),
     webPreferences: {
-      nodeIntegration: true,
+      devTools: isDev,
       preload: path.join(__dirname, "src", "main", "preload.js"),
     },
   });
 
-  win.loadFile(path.join(__dirname, "..", "renderer", "index.html"));
+  mainWindow.loadFile(path.join(__dirname, "..", "renderer", "index.html"));
 };
 
 app.on("window-all-closed", () => {
